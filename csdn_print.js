e712b5af6f13115d039ffedbf2faf37309fc7967
liangliangmax <max_translia@126.com>
2019-09-06 14:30:49 +0800

    csdn页面打印

---
+++ csdn_print.js
@@ -0,0 +1,20 @@
+(function(){
+$("aside").remove();
+$("#side").remove();
+$(".tool-box vertical").remove();
+$(".csdn-side-toolbar").remove();
+$("#csdn-toolbar").remove();
+$("#dmp_ad_58").remove();
+$(".comment-box").remove();
+$(".recommend-box").remove();
+$(".recommend-right").remove();
+$(".tool-box").remove();
+
+$(".btn-readmore").click();
+
+$("#mainBox").css('width',"1250px").css("padding","0 4px");
+
+$("main").css('width',"1250px");
+
+window.print();
+})();
